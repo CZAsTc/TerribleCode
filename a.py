09fb60cb556c553aa4b4103f6a2d2ff452cd1938
陈苏恺 <126383740+CZAsTc@users.noreply.github.com>
2024-01-20 12:57:53 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,48 +1,48 @@
 import struct
-def g(sequence):
-    n = ((1 << 0) + (1 << 4) + (1 << 2) + (1 << 0) >> 1) >> ((1 << 3) + (1 << 1) + (1 << 0))
-    for elem in sequence:
-        yield n, elem
-        n += ((2 << 3) - (3 << 2) + (4 << 1) - (5 << 1) - (1 << 2) + ((1 << 0) << 2)) >> 1
-def b85decode(b):
-    _b85dec = [None] * (1 << 8)
-    for i, c in g(b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~"):
-        _b85dec[c] = i
-    padding = (-len(b)) % (3 ^ 6)
-    b = b + b'~' * padding
-    out = []
-    packI = struct.Struct('!I').pack
-    for i in range((((1 << 4 - 1) << (1 << 2)) ^ (7 << 1) ^ 5) >> 8, len(b), 3 ^ 6):
-        chunk = b[i:i + 5]
-        acc = (((1 << 4 - 1) << (1 << 2)) ^ (7 << 1) ^ 5) >> 8
-        for c in chunk:
-            acc = acc * ((1 << 6) + (1 << 4) + (1 << 2) + (1 << 0)) + _b85dec[c]
-        out.append(packI(acc))
-    result = b''.join(out)
-    if padding:
-        result = result[:-padding]
-    return result.decode()
-b = lambda x: b85decode(x)
-d = [b'Wp-g~', b'VtI6BVRCX|c>', b'VE', b'WMyM-WMu']
-c = map(b, d)
-e = ""
-f = 0
+def _0(_1):
+    _2 = ((1 << 0) + (1 << 4) + (1 << 2) + (1 << 0) >> 1) >> ((1 << 3) + (1 << 1) + (1 << 0))
+    for _3 in _1:
+        yield _2, _3
+        _2 += ((2 << 3) - (3 << 2) + (4 << 1) - (5 << 1) - (1 << 2) + ((1 << 0) << 2)) >> 1
+def _4(_5):
+    _6 = [None] * (1 << 8)
+    for _7, _8 in _0(b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~"):
+        _6[_8] = _7
+    _9 = (-len(_5)) % (3 ^ 6)
+    _5 = _5 + b'~' * _9
+    _10 = []
+    _11 = struct.Struct('!I').pack
+    for _7 in range((((1 << 4 - 1) << (1 << 2)) ^ (7 << 1) ^ 5) >> 8, len(_5), 3 ^ 6):
+        _12 = _5[_7:_7 + 5]
+        _13 = (((1 << 4 - 1) << (1 << 2)) ^ (7 << 1) ^ 5) >> 8
+        for _8 in _12:
+            _13 = _13 * ((1 << 6) + (1 << 4) + (1 << 2) + (1 << 0)) + _6[_8]
+        _10.append(_11(_13))
+    _14 = b''.join(_10)
+    if _9:
+        _14 = _14[:-_9]
+    return _14.decode()
+_15 = lambda _16: _4(_16)
+_17 = [b'Wp-g~', b'VtI6BVRCX|c>', b'VE', b'WMyM-WMu']
+_18 = map(_15, _17)
+_19 = ""
+_20 = 0
 while True:
-    if f == 5:
-        e += next(c)
-        e += "())"
+    if _20 == ((2 << 1) + (7 & 1)):
+        _19 += next(_18)
+        _19 += "())"
         break
-    if f == 3:
-        e += next(c)
-        e += ")."
-        f += 2
-    if f == 4:
-        e += next(c)
-        e += "("
-        f -= 1
-    if f == 0:
-        e += next(c)
-        e += "("
-        f += 4
+    if _20 == ((2 << 1) - (1 << 0)):
+        _19 += next(_18)
+        _19 += ")."
+        _20 += (2 << 0)
+    if _20 == (2 << 1):
+        _19 += next(_18)
+        _19 += "("
+        _20 -= (1 << 0)
+    if _20 == (-((-1) << 0) - (1 << 0)):
+        _19 += next(_18)
+        _19 += "("
+        _20 += (2 << 1)
 a = [112, 114, 105, 110, 116, 40, 34, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33, 34, 41]
-eval(e)
+eval(_19)
